6eefc43a902e67cd7aee8990bf5f66d9e6372be9
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2021-11-19 11:07:05 -0600

    Update scala-test-example.scala

--- scala-test-example.scala
+++ scala-test-example.scala
@@ -1,9 +1,16 @@
+import org.scalatest.funsuite.AnyFunSuite
+
 object ScalaTestExample {
 
     def add(a:Int)(b:Int):Int = {
         a * b
     }
     def main(args:Array[String]):Unit = {
-        
+        test("Example test") {
+            assert(1 == 1)
+        }
+        test("Add test"){
+            assert(add(1)(1) == 1 + 1)
+        }
     }
 }
